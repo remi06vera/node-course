e47e6a642507640d2ffec1185dee8f9c08bb2183
remi <verayyone@gmail.com>
2022-05-09 09:05:48 +0800

    update

--- basic/sum.js
+++ basic/sum.js
@@ -1,10 +1,13 @@
+//目標
 // 回傳 1 + 2 + ... + n 的結果
 // function sum(n) {
 
 //     // return 結果
 //   }
+/*=========================================*/
 
-// 方法1
+// 方法1 公式解
+//O(1) big O 1
 // function sum(n) {
 //   return (n * (n + 1)) / 2;
 // }
@@ -13,14 +16,25 @@
 //2*(2+1)/2=3
 //10*(10+1)/2=55
 
-//方法2
-function sum(n) {
-  let j = 0;
-  for (let i = 1; i <= n; i++) {
-    j += i;
-  }
-  return j;
-}
+/*=========================================*/
+
+//方法2 for 方法
+////O(n) big O n
+// function sum(n) {
+//   let j = 0;
+//   for (let i = 1; i <= n; i++) {
+//     j += i;
+//   }
+//   return j;
+// }
+
+/*=========================================*/
+
+//方法3 reduce() 方法 很慢
+// function sum(n) {
+//   //[1, 2, 3,...n].reduce((sum, item) => sum + item, 0);
+//   retuen Array.from({ length: n }, (_, i) => i + 1).reduce((sum, item) => sum + item)
+// }
 
 console.log(sum(1)); // 1
 console.log(sum(2)); // 3
