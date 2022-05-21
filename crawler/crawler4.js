d4e6b6c75bf1e94ee669acb5399f595e26f8054c
remi <verayyone@gmail.com>
2022-05-21 14:29:38 +0800

    套件套件 dotenv

--- crawler/crawler4.js
+++ crawler/crawler4.js
@@ -5,15 +5,18 @@
 // 引用進來
 const mysql = require('mysql2/promise');
 
+//第三方套件 dotenv
+// 幫我們去把 .env 裡的變數讀進來
+require('dotenv').config();
 (async () => {
+  console.log('DB_HOST', process.env.DB_HOST);
   const connection = await mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'admin',
-    password: '',
-    database: 'stock',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   });
-
   let [data, fields] = await connection.execute('SELECT * FROM stocks');
   console.log(data);
 
