6d5748cc2686e76bf643c68656aa7c88c5bf0b8f
remi <verayyone@gmail.com>
2022-05-22 23:49:27 +0800

    0521HW simple-express

--- simple-express/server.js
+++ simple-express/server.js
@@ -1,14 +1,19 @@
+//步驟1：導入第三方套件-express 
 const express = require('express');
+//步驟2：利用Express 建立一個 express applications
 const app = express();
 
+//HTTP reguest(client--->請求--->server)
+//method:get,post,put,delete..
 app.get('/', (request, response, next) => {
-    respnose.send("OK");
+    response.send("Home");
 });
 
 app.get('/about', (request, response, next) => {
     response.send("About me");
 })
 
+//步驟3：告訴伺服器聽取 3000 這個 Port
 app.listen(3001, () => {
-    console.log('start server at port 3000');
+    console.log('Server running at port 3001');
 })
