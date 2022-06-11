04d06443b6c835efefa51d15c4b1ba219e231d64
remi <verayyone@gmail.com>
2022-06-11 16:19:54 +0800

    6/11新增 stock-fe

--- stock-fe/src/components/Register.js
+++ stock-fe/src/components/Register.js
@@ -1,4 +1,40 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { API_URL } from '../utils/config';
+
 const Register = () => {
+  // const [email, setEmail] = useState('');
+  // const [name, setName] = useState('');
+
+  const [member, setMember] = useState({
+    email: 'ashleylai58@gmail.com',
+    name: 'ashley',
+    password: 'testtest',
+    confirmPassword: 'testtest',
+    photo: '',
+  });
+
+  function handleChange(e) {
+    // member.email = e.target.value;  (x)
+    // 在 react 不可以直接去修改原本的 state 記憶體，
+    // 要做一塊新的記憶體，並透過 setXXX 去修改
+    setMember({ ...member, [e.target.name]: e.target.value });
+  }
+
+  async function handleSubmit(e) {
+    // 停掉預設行為
+    e.preventDefault();
+    try {
+      // axios.get(URL, params)
+      // axios.post(URL, data, params)
+      // 方法1: 當你的表單沒有圖片的時候，可以直接傳輸 json 到後端去
+      let response = await axios.post(`${API_URL}/auth/register`, member);
+      console.log(response.data);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
   return (
     <form className="bg-purple-100 h-screen md:h-full md:my-20 md:mx-16 lg:mx-28 xl:mx-40 py-16 md:py-8 px-24 text-gray-800 md:shadow md:rounded flex flex-col md:justify-center">
       <h2 className="flex justify-center text-3xl mb-6 border-b-2 pb-2 border-gray-300">註冊帳戶</h2>
@@ -6,19 +42,40 @@ const Register = () => {
         <label htmlFor="name" className="flex mb-2 w-32">
           Email
         </label>
-        <input className="w-full border-2 border-purple-200 rounded-md h-10 focus:outline-none focus:border-purple-400 px-2" type="text" id="email" name="email" />
+        <input
+          className="w-full border-2 border-purple-200 rounded-md h-10 focus:outline-none focus:border-purple-400 px-2"
+          type="text"
+          id="email"
+          name="email"
+          value={member.email}
+          onChange={handleChange}
+        />
       </div>
       <div className="mb-4 text-2xl">
         <label htmlFor="name" className="flex mb-2 w-32">
           姓名
         </label>
-        <input className="w-full border-2 border-purple-200 rounded-md h-10 focus:outline-none focus:border-purple-400 px-2" type="text" id="name" name="name" />
+        <input
+          className="w-full border-2 border-purple-200 rounded-md h-10 focus:outline-none focus:border-purple-400 px-2"
+          type="text"
+          id="name"
+          name="name"
+          value={member.name}
+          onChange={handleChange}
+        />
       </div>
       <div className="mb-4 text-2xl">
         <label htmlFor="password" className="flex mb-2 w-16">
           密碼
         </label>
-        <input className="w-full border-2 border-purple-200 rounded-md h-10 focus:outline-none focus:border-purple-400 px-2" type="password" id="password" name="password" />
+        <input
+          className="w-full border-2 border-purple-200 rounded-md h-10 focus:outline-none focus:border-purple-400 px-2"
+          type="password"
+          id="password"
+          name="password"
+          value={member.password}
+          onChange={handleChange}
+        />
       </div>
       <div className="mb-8 text-2xl">
         <label htmlFor="password" className="flex mb-2 w-32">
@@ -29,6 +86,8 @@ const Register = () => {
           type="password"
           id="confirmPassword"
           name="confirmPassword"
+          value={member.confirmPassword}
+          onChange={handleChange}
         />
       </div>
       <div className="mb-8 text-2xl">
@@ -37,7 +96,9 @@ const Register = () => {
         </label>
         <input className="w-full border-2 border-purple-200 rounded-md h-10 focus:outline-none focus:border-purple-400 px-2" type="file" id="photo" name="photo" />
       </div>
-      <button className="text-xl bg-indigo-300 px-4 py-2.5 rounded hover:bg-indigo-400 transition duration-200 ease-in">註冊</button>
+      <button className="text-xl bg-indigo-300 px-4 py-2.5 rounded hover:bg-indigo-400 transition duration-200 ease-in" onClick={handleSubmit}>
+        註冊
+      </button>
     </form>
   );
 };
--- stock-fe/src/utils/config.js
+++ stock-fe/src/utils/config.js
@@ -1,3 +1,10 @@
 // export/import => ESM 的模組管理方式
 // 設定預設值
-export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
+//用變數的方式管理後端網址
+
+// export const API_URL = process.env.REACT_APP_API_URL+"/api"|| 'http://localhost:3001/api';
+
+//再加上變數 讓預設成立
+//不要有字串出現 - - >這樣永遠成立 就不會跑到不成立的狀況中
+const BASE_URL=process.env.REACT_APP_API_URL || 'http://localhost:3001';
+export const API_URL =BASE_URL+'/api'
